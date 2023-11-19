55776ff495a117e7bc29be1b5702da5282a99b1a
Vanta11110 <11211041@student.itk.ac.id>
2023-11-19 22:44:06 +0800

    ver

---
+++ frontend/src/pages/api/index.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://satuberkas.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS"); // Tambahkan metode OPTIONS
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Sesuaikan dengan header yang diperlukan
+
+  // Tangani metode OPTIONS
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  try {
+    const response = await fetch(`http://localhost:8000/api${req.url}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}

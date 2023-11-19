a5967bf490106f34cccff7a4ea78ae9a7898b4a2
Vanta11110 <11211041@student.itk.ac.id>
2023-11-19 18:15:18 +0800

    api fix

--- frontend/src/pages/api/index.js
+++ frontend/src/pages/api/index.js
@@ -1,10 +1,15 @@
-// api/index.js
-
 export default async function handler(req, res) {
-  // Proksi permintaan ke backend Laravel
-  const response = await fetch(
-    `http://localhost:8000/api${req.url}`
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://satuberkas.vercel.app"
   );
-  const data = await response.json();
-  res.status(200).json(data);
+
+  try {
+    const response = await fetch(`http://localhost:8000/api${req.url}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }

1e8491986ebcc63bfcf29da1ccb8c46d0a6a2436
Vanta11110 <11211041@student.itk.ac.id>
2023-11-19 22:30:39 +0800

    api

--- frontend/src/pages/api/index.js
+++ frontend/src/pages/api/index.js
@@ -1,8 +1,12 @@
 export default async function handler(req, res) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://satuberkas.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://satuberkas.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
   try {
     const response = await fetch(`http://localhost:8000/api${req.url}`);

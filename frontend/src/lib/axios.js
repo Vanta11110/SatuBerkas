d38806a5bcf8859ca9465328505ed7a34fcb3e68
Vanta11110 <11211041@student.itk.ac.id>
2023-11-19 22:27:04 +0800

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,10 @@
 import Axios from "axios";
 
-const csrfToken = document.head.querySelector(
-  'meta[name="csrf-token"]'
-).content;
 
 const axios = Axios.create({
   baseURL: process.env.BACKEND_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
-    "X-CSRF-TOKEN": csrfToken,
   },
   withCredentials: true,
 });

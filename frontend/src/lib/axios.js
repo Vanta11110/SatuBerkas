af9a22d6d8c2fa2fb2497bf305ff7c6a13031f4c
Vanta11110 <11211041@student.itk.ac.id>
2023-11-24 23:46:13 +0800

    update axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import Axios from "axios";
 const axios = Axios.create({
   baseURL: process.env.BACKEND_URL,
   headers: {
-    "X-Requested-With": "XMLHttpRequest",
+    "X-Requested-With": "XMLHttpsRequest",
   },
   withCredentials: true,
   withXSRFToken: true

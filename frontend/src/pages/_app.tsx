f76e5a8f602830602dee0ce014a29228e9ec5fe9
Vanta11110 <11211041@student.itk.ac.id>
2023-11-25 22:06:30 +0800

    update app

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,17 +2,16 @@ import '@styles/globals.scss'
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { SSRProvider } from 'react-bootstrap'
 import { ProgressBar } from '@components/ProgressBar'
 
 // config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SSRProvider>
+    <>
       <ProgressBar />
       <Component {...pageProps} />
-    </SSRProvider>
+    </>
 
   )
 }

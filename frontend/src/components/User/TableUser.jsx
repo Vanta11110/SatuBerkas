fe5aae4e5b0c1cd61dd7f9309157ffefb6c160d9
Vanta11110 <11211041@student.itk.ac.id>
2023-11-27 04:27:53 +0800

    update delete admin

--- frontend/src/components/User/TableUser.jsx
+++ frontend/src/components/User/TableUser.jsx
@@ -36,7 +36,7 @@ const User = () => {
     let params = { _method: "delete" };
 
     axios
-      .post("/api/admin" + id, params)
+      .post("/api/admin/" + id, params)
       .then((response) => {
         console.log("delete berhasil")
         setToast({

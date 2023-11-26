11b4eee3ec664051d96b83ff23bf7e5d338e93b3
Vanta11110 <11211041@student.itk.ac.id>
2023-11-26 18:10:26 +0800

    update options

--- frontend/src/components/Options/RtOptions.jsx
+++ frontend/src/components/Options/RtOptions.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const RTOptions = () => {
   const rtOptions = Array.from({ length: 59 }, (_, index) => ({
     value: (index + 1).toString().padStart(2, "0"),
-    label: `RT ${index + 1}`,
+    label: `RT ${(index + 1).toString().padStart(2, "0")}`,
   }));
 
   return rtOptions;
